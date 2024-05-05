869f04642f3e6751a6e2bdb65d74bd7075c228e2
ayushchy <ayush.choudhary27@gmail.com>
2024-05-05 14:31:37 +0530

    trying the test branch

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Eat -`{'>'}` Sleep -`{'>'}` Code -`{'>'}` Repeat
         </a>
       </header>
     </div>
